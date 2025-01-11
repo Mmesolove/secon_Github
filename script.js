5452ca60f94868593f1bc15df56efba651d0b55d
Mmeso Love <mmesolove2002@gmail.com>
2025-01-11 22:56:51 +0100

    i added shola as a comment

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function randomColor(){
  const red = Math.round(Math.random () * 255)
 const green = Math.round(Math.random () * 255)
 const blue = Math.round(Math.random () * 255)
-
+//shola
 const color =`rgb(${red}, ${green}, ${blue})`
 body.style.backgroundColor = color;
 
